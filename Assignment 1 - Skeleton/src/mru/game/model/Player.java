7486e5cd7a66a92cb6e5a7e74b2c0c1bd7552102
daraakitoyemru <dakit711@mtroyal.ca>
2023-02-10 22:04:33 -0700

    Implements player model class

--- Assignment 1 - Skeleton/src/mru/game/model/Player.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Player.java
@@ -6,4 +6,43 @@ public class Player {
 	 * This class represent each player record in the Database
 	 * It is basically a model class for each record in the txt file
 	 */
+	
+	public String name;
+	public int numOfWins;
+	public int balance;
+	
+	public Player(String name, int numOfWins, int balance) {
+		this.name = name;
+		this.numOfWins = numOfWins;
+		this.balance = balance;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setWins(int numOfWins) {
+		this.numOfWins = numOfWins;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getWins() {
+		return numOfWins;
+	}
+	
+	public int setBalance() {
+		return balance;
+	}
+	
+	public String format() {
+		return name+","+numOfWins+","+balance;
+	}
 }
